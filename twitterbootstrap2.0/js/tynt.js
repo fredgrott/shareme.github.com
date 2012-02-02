f66e5dc34a90480ca837917e535968810617b097
Fred Grott <fred.grott@gmail.com>
2012-02-02 15:42:51 -0600

    did a change over to first release of twitterbootstrap2.0, minor corrections, and minor design tweaks

---
+++ twitterbootstrap2.0/js/tynt.js
@@ -0,0 +1,7 @@
+
+if(document.location.protocol=='http:'){
+ var Tynt=Tynt||[];Tynt.push('bveKsWa9er4AM8acwqm_6r');Tynt.i={"ap":"Read more:","cc":"2","b":true};
+ (function(){var s=document.createElement('script');s.async="async";s.type="text/javascript";s.src='http://tcr.tynt.com/ti.js';var h=document.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);})();
+}
+
+
