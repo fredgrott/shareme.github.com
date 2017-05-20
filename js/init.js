dd37cd318af88561e6193dcd5f8d27804d2c6291
Fred Grott <fred.grott@gmail.com>
2017-05-20 11:00:42 -0500

    made change to design direction
    made design direction change

--- js/init.js
+++ js/init.js
@@ -1,16 +1,38 @@
 (function($){
   $(function(){
+  "use strict";
 
-    $('.button-collapse').sideNav();
-
-
-    var $container = $('#masonry-grid');
-    // initialize
-    $container.masonry({
-      columnWidth: '.col',
-      itemSelector: '.col',
-    });
+  var window_width = $(window).width();
 
 
-  }); // end of document ready
+
+
+
+    $('.carousel.carousel-slider').carousel({full_width: true});
+    $('.carousel').carousel();
+    $('.slider').slider({full_width: true});
+    $('.parallax').parallax();
+    $('.modal').modal();
+    $('.scrollspy').scrollSpy();
+    $('.button-collapse').sideNav({'edge': 'left', menuWidth: '240'});
+    $('.datepicker').pickadate({selectYears: 20});
+    $('select').not('.disabled').material_select();
+
+    // Detect touch screen and enable scrollbar if necessary
+      function is_touch_device() {
+        try {
+          document.createEvent("TouchEvent");
+          return true;
+        }
+        catch (e) {
+          return false;
+        }
+      }
+      if (is_touch_device()) {
+        $('#nav-mobile').css({
+          overflow: 'auto'
+        })
+      }
+
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
