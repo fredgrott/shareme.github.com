057dd43c0a4a1437d89091eb939fa82bb0964bc0
Fred Grott <fred.grott@gmail.com>
2017-10-17 13:16:23 -0500

    getting ready to rumble

---
+++ assets/js/init.js
@@ -0,0 +1,47 @@
+(function($){
+  $(function(){
+
+    var window_width = $(window).width();
+
+
+
+
+
+    
+
+
+
+    // Detect touch screen and enable scrollbar if necessary
+    function is_touch_device() {
+      try {
+        document.createEvent("TouchEvent");
+        return true;
+      } catch (e) {
+        return false;
+      }
+    }
+    if (is_touch_device()) {
+      $('#nav-mobile').css({ overflow: 'auto'});
+    }
+
+
+   $(".dropdown-button").dropdown();
+
+    // Plugin initialization
+    $('.carousel.carousel-slider').carousel({fullWidth: true});
+    $('.carousel').carousel();
+    $('.slider').slider({full_width: true});
+    $('.parallax').parallax();
+    $('.modal').modal();
+    $('.scrollspy').scrollSpy();
+    $('.button-collapse').sideNav({'edge': 'left'});
+    $('.datepicker').pickadate({selectYears: 20});
+    $('.timepicker').pickatime();
+    $('select').not('.disabled').material_select();
+
+
+
+
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
