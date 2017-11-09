9cf7dd616d3b419bd41e482b50e20a6622745aed
Fred Grott <fred.grott@gmail.com>
2017-11-09 06:40:25 -0600

    v1
    v1

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,47 +1,9 @@
-(function($){
-  $(function(){
-
-    var window_width = $(window).width();
 
 
 
 
 
-    
 
+	       $('body').bootstrapMaterialDesign();
 
-
-    // Detect touch screen and enable scrollbar if necessary
-    function is_touch_device() {
-      try {
-        document.createEvent("TouchEvent");
-        return true;
-      } catch (e) {
-        return false;
-      }
-    }
-    if (is_touch_device()) {
-      $('#nav-mobile').css({ overflow: 'auto'});
-    }
-
-
-   $(".dropdown-button").dropdown();
-
-    // Plugin initialization
-    $('.carousel.carousel-slider').carousel({fullWidth: true});
-    $('.carousel').carousel();
-    $('.slider').slider({full_width: true});
-    $('.parallax').parallax();
-    $('.modal').modal();
-    $('.scrollspy').scrollSpy();
-    $('.button-collapse').sideNav({'edge': 'left'});
-    $('.datepicker').pickadate({selectYears: 20});
-    $('.timepicker').pickatime();
-    $('select').not('.disabled').material_select();
-
-
-
-
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+  
